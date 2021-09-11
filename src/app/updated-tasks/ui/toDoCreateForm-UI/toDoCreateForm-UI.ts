e465433796a70150fd2ad5fd9dd05d0465658f25
JuliaClifford <69593248+JuliaClifford@users.noreply.github.com>
2021-09-11 16:18:47 +0300

    Date format changed

--- src/app/updated-tasks/ui/toDoCreateForm-UI/toDoCreateForm-UI.ts
+++ src/app/updated-tasks/ui/toDoCreateForm-UI/toDoCreateForm-UI.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ITaskItem } from "../../../shared/interfaces/ITaskItem";
 import { taskStatuses } from "../../../shared/constants/taskStatuses";
 import { AddNewTask } from "../../../store/actions/actions";
